5c94ff3aabb6442d75ab2036871de261d37bae56
Sebastian Bechtold <mr.bechtold@gmail.com>
2021-01-22 12:24:46 +0100

    - Errors in WmsCapabilitis::getBoundingBox() are now caught

--- WmsCapabilities.js
+++ WmsCapabilities.js
@@ -20,20 +20,26 @@ class WmsCapabilities {
         return result;
     }
     getBoundingBox() {
-        // TODO: Find out how exactly the bounding box which is return here is defined.
-        // Is it  the total min/max coordinates of all layers?
-        let elems_bbox = this.getElementsByTagname(this.document.elements[0].elements[1].elements[2], "EX_GeographicBoundingBox");
-        if (elems_bbox == null) {
+        try {
+            // TODO: Find out how exactly the bounding box which is return here is defined.
+            // Is it  the total min/max coordinates of all layers?
+            let elems_bbox = this.getElementsByTagname(this.document.elements[0].elements[1].elements[2], "EX_GeographicBoundingBox");
+            if (elems_bbox == null) {
+                return null;
+            }
+            let elem_bbox = elems_bbox[0];
+            // TODO: 3 Properly check for null here
+            let west_string = this.getElementsByTagname(elem_bbox, "westBoundLongitude")[0].elements[0].text;
+            let east_string = this.getElementsByTagname(elem_bbox, "eastBoundLongitude")[0].elements[0].text;
+            let south_string = this.getElementsByTagname(elem_bbox, "southBoundLatitude")[0].elements[0].text;
+            let north_string = this.getElementsByTagname(elem_bbox, "northBoundLatitude")[0].elements[0].text;
+            let bbox = [parseFloat(west_string), parseFloat(south_string), parseFloat(east_string), parseFloat(north_string)];
+            return bbox;
+        }
+        catch (e) {
+            console.log(e);
             return null;
         }
-        let elem_bbox = elems_bbox[0];
-        // TODO: 3 Properly check for null here
-        let west_string = this.getElementsByTagname(elem_bbox, "westBoundLongitude")[0].elements[0].text;
-        let east_string = this.getElementsByTagname(elem_bbox, "eastBoundLongitude")[0].elements[0].text;
-        let south_string = this.getElementsByTagname(elem_bbox, "southBoundLatitude")[0].elements[0].text;
-        let north_string = this.getElementsByTagname(elem_bbox, "northBoundLatitude")[0].elements[0].text;
-        let bbox = [parseFloat(west_string), parseFloat(south_string), parseFloat(east_string), parseFloat(north_string)];
-        return bbox;
     }
     getVersion() {
         return this.document.elements[0].attributes.version;
--- src/WmsCapabilities.ts
+++ src/WmsCapabilities.ts
@@ -2,15 +2,15 @@ import * as xmljs from 'xml-js'
 
 export class WmsCapabilities {
 
-    document! : any
+    document!: any
 
-    constructor(private xmlString : string) {        
+    constructor(private xmlString: string) {
 
         this.document = JSON.parse(xmljs.xml2json(xmlString))
     }
 
 
-    getElementsByTagname(parent : any, tagName : string) : Array<any> | null {
+    getElementsByTagname(parent: any, tagName: string): Array<any> | null {
 
         if (parent == undefined || parent.elements == undefined || parent.elements.length == 0) {
             return null
@@ -18,7 +18,7 @@ export class WmsCapabilities {
 
         let result = []
 
-        for(let element of parent.elements) {
+        for (let element of parent.elements) {
             if (element.type == "element" && element.name == tagName) {
                 result.push(element)
             }
@@ -26,34 +26,40 @@ export class WmsCapabilities {
 
         return result
     }
-   
 
-    getBoundingBox() : any {
 
-        // TODO: Find out how exactly the bounding box which is return here is defined.
-        // Is it  the total min/max coordinates of all layers?
-        let elems_bbox = this.getElementsByTagname(this.document.elements[0].elements[1].elements[2], "EX_GeographicBoundingBox")
+    getBoundingBox(): any {
 
-        if (elems_bbox == null) {
+        try {
+            // TODO: Find out how exactly the bounding box which is return here is defined.
+            // Is it  the total min/max coordinates of all layers?
+            let elems_bbox = this.getElementsByTagname(this.document.elements[0].elements[1].elements[2], "EX_GeographicBoundingBox")
+
+            if (elems_bbox == null) {
+                return null
+            }
+
+            let elem_bbox = elems_bbox[0]
+
+            // TODO: 3 Properly check for null here
+            let west_string = this.getElementsByTagname(elem_bbox, "westBoundLongitude")![0].elements[0].text
+            let east_string = this.getElementsByTagname(elem_bbox, "eastBoundLongitude")![0].elements[0].text
+            let south_string = this.getElementsByTagname(elem_bbox, "southBoundLatitude")![0].elements[0].text
+            let north_string = this.getElementsByTagname(elem_bbox, "northBoundLatitude")![0].elements[0].text
+
+
+            let bbox = [parseFloat(west_string), parseFloat(south_string), parseFloat(east_string), parseFloat(north_string)]
+
+            return bbox
+        }
+        catch (e) {
+            console.log(e)
             return null
         }
-
-        let elem_bbox = elems_bbox[0]
-
-        // TODO: 3 Properly check for null here
-        let west_string = this.getElementsByTagname(elem_bbox, "westBoundLongitude")![0].elements[0].text
-        let east_string = this.getElementsByTagname(elem_bbox, "eastBoundLongitude")![0].elements[0].text
-        let south_string = this.getElementsByTagname(elem_bbox, "southBoundLatitude")![0].elements[0].text
-        let north_string = this.getElementsByTagname(elem_bbox, "northBoundLatitude")![0].elements[0].text
-
-
-        let bbox = [parseFloat(west_string), parseFloat(south_string), parseFloat(east_string), parseFloat(north_string)]
-       
-        return bbox
     }
 
 
-    getVersion() : string {
+    getVersion(): string {
         return this.document.elements[0].attributes.version
     }
 }
